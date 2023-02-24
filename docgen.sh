eabf20bfb818695f82adc191cb7bc7441124bec8
leenzstra <leenzstra@gmail.com>
2023-02-24 19:06:24 +0500

    add docgen

---
+++ docgen.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+go install github.com/princjef/gomarkdoc/cmd/gomarkdoc@latest
+
+gomarkdoc --output ".\docs\doc.md" \
+                            ".\cmd" \
+                            ".\internal\utils" \
+                            ".\internal\responses" \
+                            ".\internal\models" 
+                            ".\internal\middlewares" \
+                            ".\internal\keypair" \
+                            ".\internal\db" \
+                            ".\internal\config" \
+                            ".\internal\collections" \
+                            ".\internal\api\content" \
+                            ".\internal\api\info" \
+                            ".\internal\api\license" \
+                            /
