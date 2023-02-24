8ab375489b96909db79ff1b0cae5c5f7b822ea2c
leenzstra <leenzstra@gmail.com>
2023-02-24 19:21:00 +0500

    fix

--- docgen.sh
+++ docgen.sh
@@ -2,22 +2,24 @@
 
 go install github.com/princjef/gomarkdoc/cmd/gomarkdoc@latest
 
+ls
+
 packagelist=(
-    ".\cmd"
-    ".\internal\utils"
-    ".\internal\responses"
-    ".\internal\models"                  
-    ".\internal\middlewares"
-    ".\internal\keypair" 
-    ".\internal\db" 
-    ".\internal\config" 
-    ".\internal\collections" 
-    ".\internal\api\content" 
-    ".\internal\api\info"
-    ".\internal\api\license" 
+    "./cmd"
+    "./internal/utils"
+    "./internal/responses"
+    "./internal/models"                  
+    "./internal/middlewares"
+    "./internal/keypair" 
+    "./internal/db" 
+    "./internal/config" 
+    "./internal/collections" 
+    "./internal/api/content" 
+    "./internal/api/info"
+    "./internal/api/license" 
 )
 
 
-gomarkdoc --output ".\docs\doc.md" ${packagelist[@]}
+gomarkdoc --output "./docs/doc.md" ${packagelist[@]}
                             
                             
