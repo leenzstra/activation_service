f906f01b956ec71b7663d8a080bc89dac1a44c8f
leenzstra <leenzstra@users.noreply.github.com>
2023-03-04 05:57:13 +0000

    deploy: 01951e8b1f41f9ab04c0ab8e8b4326182068d00f

--- internal/api/content/controller.go
+++ internal/api/content/controller.go
@@ -3,6 +3,7 @@ package content
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/leenzstra/activation_service/internal/db"
+	"github.com/gofiber/keyauth/v2"
 )
 
 type handler struct {
@@ -10,12 +11,22 @@ type handler struct {
 }
 
 func RegisterRoutes(app *fiber.App, db *db.Database) {
-	// h := &handler{
-	// 	DB: db,
-	// }
+	h := &handler{
+		DB: db,
+	}
+
+	var licenseAuth = keyauth.New(keyauth.Config{
+		KeyLookup: "header:License",
+		Validator: func(c *fiber.Ctx, key string) (bool, error) {
+			if key == h.DB.Config.ApiKey {
+				return true, nil
+			}
+			return false, fiber.ErrUnauthorized
+		},
+	})
 
 	routes := app.Group("/api/content")
-	files := routes.Group("/files")
+	files := routes.Group("/files", licenseAuth)
 
 	files.Static("/", "./files")
 	// routes.Get("/subjects", h.GetSubjects)
